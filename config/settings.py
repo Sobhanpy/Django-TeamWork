899608c9eed329a33db9bd688643ab4b354ccfdf
Sobhan Moradi <sobhan.mrds@gmail.com>
2023-11-28 20:48:23 +0330

    config url

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 STATIC_ROOT = STATIC_ROOT = BASE_DIR.joinpath('static')
 
