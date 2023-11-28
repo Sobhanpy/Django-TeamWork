5862c7381c8c27314678b013c7f1f7d2a1539599
Hesam Gh <110620352+Hesamghadami@users.noreply.github.com>
2023-11-29 00:13:14 +0330

    dvsdv

--- blogs/api/V1/urls.py
+++ blogs/api/V1/urls.py
@@ -5,7 +5,7 @@ from .views import *
 app_name = 'api-v1'
 
 urlpatterns = [
-    path("Posts/", post_api_view,name='posts'),
-    path("Post-detail/<int:pk>", post_api_detail_view,name='post-detail'),
+    path("Posts/", post_api_view, name='posts'),
+    path("Post-detail/<int:pk>", post_api_detail_view, name='post-detail'),
 
 ]
